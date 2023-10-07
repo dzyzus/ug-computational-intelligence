3a2d32ab753cb56de4ddbb85094b147cd97cb4da
dzyzus <dzyzusek@gmail.com>
2023-10-07 17:12:50 +0200

    First ex.
    Prime numbers func

---
+++ Lab1/main.py
@@ -0,0 +1,22 @@
+from Exercises import PrimeNumber
+
+"""
+prime(3)
+Wynik: True
+prime(4)
+Wynik: False
+prime(49)
+Wynik: False
+"""
+
+PrimeNumber.prime(3)
+PrimeNumber.prime(4)
+PrimeNumber.prime(49)
+
+"""
+Przykład działania:
+select_primes([3, 6, 11, 25, 19])
+Wynik: [3, 11, 19]
+"""
+
+PrimeNumber.select_primes([3, 6, 11, 25, 19])
