80e9e0bad89c32ab51e17a987a7e955c43521863
dzyzus <dzyzusek@gmail.com>
2023-11-01 21:27:19 +0100

    Lab3 ex1

--- Lab3/Exercises/pso_metal_alloy.py
+++ Lab3/Exercises/pso_metal_alloy.py
@@ -1,23 +1,38 @@
-# Import modules
-import numpy as np
-import os
+import numpy
+import math
 from matplotlib import pyplot as plt
-# Import PySwarms
 import pyswarms as ps
 from pyswarms.utils.functions import single_obj as fx
 from pyswarms.utils.plotters import plot_cost_history
 from pyswarms.utils.plotters.plotters import plot_contour
-from pyswarms.utils.plotters.formatters import Mesher 
-# Set-up hyperparameters
-options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+from pyswarms.utils.plotters.formatters import Mesher
 
+# Set-up hyperparameters
+options = {'c1': 0.5, 'c2': 0.3, 'w': 0.5}
+
+x_min = numpy.zeros(6)
+x_max = numpy.ones(6)
+my_bounds = (x_min, x_max)
 
 # Call instance of PSO
-optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)
+optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=6, options=options, bounds=my_bounds)
+
+# Endurance func
+def endurance(params):
+    x, y, z, u, v, w = params
+    return -(math.exp(-2 * (y - math.sin(x)) ** 2) + math.sin(z * u) + math.cos(v * w))
+
+# Compute whole swarm
+def f(x):
+    results = []
+    for particle in x:
+        result = endurance(particle)
+        results.append(result)
+    return numpy.array(results)
 
 def run_pso():
     # Perform optimization
-    cost, pos = optimizer.optimize(fx.sphere, iters=200)
+    cost, pos = optimizer.optimize(f, iters=1000)
     # Obtain cost history from optimizer instance
     draw_plot()
     animation()
@@ -30,7 +45,7 @@ def draw_plot():
     plt.show()
 
 def animation():
-    options = {'c1':0.5, 'c2':0.3, 'w':0.5} 
+    options = {'c1': 0.5, 'c2': 0.3, 'w': 0.5}
     optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)
     optimizer.optimize(fx.sphere, iters=50) 
     # tworzenie animacji 
--- Lab3/main.py
+++ Lab3/main.py
@@ -1,6 +1,10 @@
 #from LabFiles import pso_sphere
-from LabFiles import aco_tsp
-from Exercises import aco_tsp_ex
+#from LabFiles import aco_tsp
+
+#from Exercises import aco_tsp_ex
+from Exercises import pso_metal_alloy
 
 #pso_sphere.run_pso()
-aco_tsp_ex.run_aco()
+#aco_tsp_ex.run_aco()
+
+pso_metal_alloy.run_pso()
