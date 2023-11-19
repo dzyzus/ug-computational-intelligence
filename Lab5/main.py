ce3437ddc27ed3ef3265e6747b7fdba02d1a9bf4
dzyzus <dzyzusek@gmail.com>
2023-11-19 17:28:03 +0100

    finish ex3

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,7 +1,7 @@
-from Exercises import ex_1
-from Exercises import ex_2
-#from Exercises import ex_3
+#from Exercises import ex_1
+#from Exercises import ex_2
+from Exercises import ex_3
 
-ex_1.run()
-ex_2.run()
-#ex_3.run()
+#ex_1.run()
+#ex_2.run()
+ex_3.run()
