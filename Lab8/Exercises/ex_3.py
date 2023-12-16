b5306b6999d11ae927608454e92b6f1ac9050407
dzyzus <dzyzusek@gmail.com>
2023-12-16 12:56:35 +0100

    added exercises

---
+++ Lab8/Exercises/ex_3.py
@@ -0,0 +1,20 @@
+'''
+Zadanie 3
+Wykonaj następujące polecenia związane z sieciami rekurencyjnymi:
+a) Uruchom i przeanalizuj pliki z wykładu o sieciach rekurencyjnych:
+• rnn01.py (prosta demonstracja)
+• rnn02.py (Badanie jak działa sieć rekurencyjna)
+• rnn03.py (Przewidywanie liczby plam na słońcu w danym miesiącu -RNN)
+• lstm01.py (Przewidywanie liczby plam na słońcu w danym miesiącu - LSTM)
+• lstm02.py (Uczenie generowania tekstu przez LSTM litera po literze)
+• lstm03.py (Uruchomienie generatora LSTM z poprzedniego zadania)
+• lstm04.py (Uczenie generowania tekstu przez LSTM słowo po słowie)
+• lstm05.py (Uruchomienie generatora LSTM z poprzedniego zadania)
+Uwaga: w zadaniach lstm02.py i lstm04.py uczenie może trwać wiele godzin.
+Znacznie zmniejsz liczbę epok, by trenowanie trwało parę minut.
+b) Zademonstruj jak działają generatory tekstu (lstm03 i lstm05) po Twoim krótkim
+treningu.
+c) Dotrenuj model z lstm02 i lstm04 o parę epok, wykorzystując jako bazę
+startową już wytrenowany przez Ciebie model zapisany w pliku hdf5. Następnie
+pokaż, czy generowany tekst jest trochę lepszy.
+'''
---
+++ Lab8/Exercises/ex_4.py
@@ -0,0 +1,12 @@
+'''
+Zadanie 4
+To zadanie jest dość luźne, ale ciekaw jestem Państwa znalezisk i eksperymentów.
+• Znajdź gotowy wytrenowany model do generacji zdjęć lub generacji tekstu w
+Internecie. Można skorzystać z modeli dostępnych na githubie, zamieszczanych
+przez wielu twórców, albo zajrzeć na stronę https://huggingface.co/ która
+oferuje dostęp do wielu modeli.
+• Zademonstruj jego działanie: wygeneruj kilka zdjęć lub fragmenty tekstu.
+Niektóre bardziej obciążające CPU/GPU można spróbować odpalić na google
+collab.
+• Krótko opowiedz o tym modelu (struktura, pamięć).
+'''
